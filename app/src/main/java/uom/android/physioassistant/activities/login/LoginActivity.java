d8dbde2eb59f81142748a38a38c883ffc880f0f1
Archontis Kostis <arxontisk02@gmail.com>
2023-05-08 14:15:33 +0300

    Create doctor, patient and admin home activities

--- app/src/main/java/uom/android/physioassistant/activities/login/LoginActivity.java
+++ app/src/main/java/uom/android/physioassistant/activities/login/LoginActivity.java
@@ -7,28 +7,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import uom.android.physioassistant.R;
-import uom.android.physioassistant.activities.admin.AdminHomeActivity;
+import uom.android.physioassistant.activities.admin.AdminActivity;
+import uom.android.physioassistant.activities.doctor.DoctorActivity;
+import uom.android.physioassistant.activities.patient.PatientActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText username_input;
-    EditText password_input;
+    EditText usernameInput;
+    EditText passwordInput;
+    TextView errorMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_login);
+        setContentView(R.layout.activity_login);
 
-        this.username_input = findViewById(R.id.username_input);
-        this.password_input = findViewById(R.id.password_input);
-
-
+        this.usernameInput = findViewById(R.id.username_input);
+        this.passwordInput = findViewById(R.id.password_input);
+        this.errorMsg = findViewById(R.id.error_msg);
     }
 
     public void handleLogin(View view) {
-        String username = String.valueOf(username_input.getText());
-        String password = String.valueOf(password_input.getText());
+        String username = String.valueOf(usernameInput.getText());
+        String password = String.valueOf(passwordInput.getText());
         Log.i("Login", "Username: " + username + " Password: " + password);
 
         // TO DO: Call login endpoint
@@ -37,12 +40,23 @@ public class LoginActivity extends AppCompatActivity {
 
         if (role.equalsIgnoreCase("admin")) {
             Log.i("Login", "Admin Login");
-            next_activity = new Intent(this, AdminHomeActivity.class);
+            next_activity = new Intent(this, AdminActivity.class);
             startActivity(next_activity);
         }
-        else if (role.equalsIgnoreCase("patient"))
+        else if (role.equalsIgnoreCase("patient")) {
+            next_activity = new Intent(this, PatientActivity.class);
+            startActivity(next_activity);
             Log.i("Login", "Patient Login");
-        else if (role.equalsIgnoreCase("doctor"))
-                Log.i("Login", "Doctor Login");
+        }
+        else if (role.equalsIgnoreCase("doctor")) {
+            Log.i("Login", "Doctor Login");
+            next_activity = new Intent(this, DoctorActivity.class);
+            startActivity(next_activity);
+        }
+        else {
+            Log.w("Login", "Invalid Credentials");
+            this.errorMsg.setVisibility(View.VISIBLE);
+        }
+
     }
 }
