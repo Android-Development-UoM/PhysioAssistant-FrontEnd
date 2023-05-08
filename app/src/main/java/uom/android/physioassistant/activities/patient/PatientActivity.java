fdd20b4cda7af2a558b1c407457ea43512a9f268
giorgos-tsak <giorgos.tsakalidis2002@gmail.com>
2023-05-08 23:18:12 +0300

    created home screen and menu bar

--- app/src/main/java/uom/android/physioassistant/activities/patient/PatientActivity.java
+++ app/src/main/java/uom/android/physioassistant/activities/patient/PatientActivity.java
@@ -1,7 +1,11 @@
 package uom.android.physioassistant.activities.patient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 import uom.android.physioassistant.R;
@@ -12,5 +16,33 @@ public class PatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+
+        replaceFragment(new HomeFragment());
+
+
+    }
+
+    public void replaceFragment(Fragment fragment){
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        System.out.println(getSupportFragmentManager().getBackStackEntryCount());
+        if(getSupportFragmentManager().getBackStackEntryCount()!=1){
+            super.onBackPressed();
+        }
+        else{
+            Intent a = new Intent(Intent.ACTION_MAIN);
+            a.addCategory(Intent.CATEGORY_HOME);
+            a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(a);
+        }
     }
 }
