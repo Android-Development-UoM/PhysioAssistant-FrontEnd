1189105f4af8145d6893ec6416dbebe95e0e0135
Archontis Kostis <arxontisk02@gmail.com>
2023-05-08 13:06:40 +0300

    Implement button listeners

---
+++ app/src/main/java/uom/android/physioassistant/activities/login/AdminLoginActivity.java
@@ -0,0 +1,16 @@
+package uom.android.physioassistant.activities.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uom.android.physioassistant.R;
+
+public class AdminLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
