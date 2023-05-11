3ad8d78bdf539e9c331725a74bdd9a95db27faeb
Archontis Kostis <arxontisk02@gmail.com>
2023-05-11 15:09:48 +0300

    Update Doctor.java

--- app/src/main/java/uom/android/physioassistant/models/Doctor.java
+++ app/src/main/java/uom/android/physioassistant/models/Doctor.java
@@ -12,6 +12,30 @@ public class Doctor extends User {
         this.address = address;
     }
 
+    public String getAfm() {
+        return afm;
+    }
+
+    public void setAfm(String afm) {
+        this.afm = afm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "Doctor{" +
