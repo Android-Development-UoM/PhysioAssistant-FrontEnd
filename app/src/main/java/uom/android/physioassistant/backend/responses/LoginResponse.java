138dad7e1a4ee71ab961d1013a8924a0bc8183fb
Archontis Kostis <arxontisk02@gmail.com>
2023-05-11 12:28:12 +0300

    Update LoginResponse.java

--- app/src/main/java/uom/android/physioassistant/backend/responses/LoginResponse.java
+++ app/src/main/java/uom/android/physioassistant/backend/responses/LoginResponse.java
@@ -1,33 +1,33 @@
 package uom.android.physioassistant.backend.responses;
 
 import uom.android.physioassistant.models.User;
-
 public class LoginResponse {
     private User user;
     private String errorMessage;
-    private String userRole;
-
-    public LoginResponse() {
+    public LoginResponse(User user) {
+        this.user = user;
     }
+
+    public LoginResponse(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public LoginResponse() {}
 
     public User getUser() {
         return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public String getErrorMessage() {
         return errorMessage;
     }
 
-    public String getUserRole() {
-        return userRole;
-    }
-
-    @Override
-    public String toString() {
-        return "LoginResponse{" +
-                "user=" + user +
-                ", errorMessage='" + errorMessage + '\'' +
-                ", userRole='" + userRole + '\'' +
-                '}';
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }
+
