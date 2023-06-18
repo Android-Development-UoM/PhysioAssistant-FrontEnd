e2c5a0c1cab021034f5f831413c90a357f64f774
Archontis Kostis <arxontisk02@gmail.com>
2023-06-18 19:55:08 +0300

    Remove unused imports

--- app/src/main/java/uom/android/physioassistant/activities/SplashActivity.java
+++ app/src/main/java/uom/android/physioassistant/activities/SplashActivity.java
@@ -1,31 +1,17 @@
 package uom.android.physioassistant.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
-
-import java.util.ArrayList;
 
 import uom.android.physioassistant.R;
-import uom.android.physioassistant.activities.admin.AdminActivity;
-import uom.android.physioassistant.activities.doctor.DoctorActivity;
 import uom.android.physioassistant.activities.login.LoginActivity;
-import uom.android.physioassistant.activities.patient.PatientActivity;
-import uom.android.physioassistant.backend.datamanager.DataManager;
-import uom.android.physioassistant.backend.events.DoctorsLoadedEvent;
-import uom.android.physioassistant.backend.events.PhysioActionsLoadedEvent;
-import uom.android.physioassistant.models.Admin;
-import uom.android.physioassistant.models.Doctor;
-import uom.android.physioassistant.models.Patient;
-import uom.android.physioassistant.models.PhysioAction;
 
 public class SplashActivity extends AppCompatActivity {
 
